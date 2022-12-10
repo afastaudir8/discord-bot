0c01d5298af1f345b0b0e68bd8f198340a06a84c
afastaudir8 <ervd516@gmail.com>
2022-12-10 08:04:39 +0000

    Update the 8ball command

--- cogs/Random.py
+++ cogs/Random.py
@@ -17,6 +17,8 @@ class Random(commands.Cog):
     async def eightball(self, ctx, message=None):
         pick = choice(["As I see it, yes.", "Ask again later.", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.", "Don’t count on it.", "It is certain.", "It is decidedly so.", "Most likely.", "My reply is no.", "My sources say no.", "Outlook not so good.", "Outlook good.", "Reply hazy, try again.", "Signs point to yes.", "Very doubtful.", "Without a doubt.", "Yes.", "Yes – definitely.", "You may rely on it."])
         embed = discord.Embed(title = '8ball', description = pick)
+        embed.set_footer(text=f"Requested by {ctx.message.author}. Orignal message: {message}")
+        await ctx.message.delete()
         await ctx.send(embed=embed)
 
 #    @commands.command()
