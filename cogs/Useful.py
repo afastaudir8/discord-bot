2de3670d0e8989d1145905386df3642efa2e3f62
afastaudir8 <ervd516@gmail.com>
2022-12-11 21:44:04 +0000

    Updated the suicidehotlines command

--- cogs/Useful.py
+++ cogs/Useful.py
@@ -27,7 +27,9 @@ class Useful(commands.Cog):
     embed = discord.Embed(title = 'Credits', description = f'Bot created by <@478212283701526529> using the discord.py library. Other libraries include Translator, goslate, datetime, and asyncio \n Check out the source code here: https://github.com/afastaudir8/discord-bot', url='https://github.com/afastaudir8/discord-bot', colour=0x00FFF3)
     embed.set_footer(text=f"Requested by {interaction.user.name}. Current bot version: 1.2.0.")
     await interaction.response.send_message(embed=embed)
-  @commands.command()
+
+#  @commands.command()
+  @app_commands.command(name='suicidehotlines', description='Displays a Wikipedia page for suicide hotlines.')
   async def suicidehotlines(self, ctx):
     embed = discord.Embed(title='Suicide Prevention Hotlines', description=f"You're not alone  \n We all care about you, please **do not** go through with it. \nCall one of the crisis hotlines listed in the Wikipedia article linked in the title depending on where you are. You could also ping Ash (<@708094727588937799>) for help.", url='https://en.wikipedia.org/wiki/List_of_suicide_crisis_lines', color=0xff0000)
     embed.set_footer(text=f'Requested by {ctx.message.author}')
