ff0b02e1a1aee825223b7a25cb06e922e7a701ee
afastaudir8 <ervd516@gmail.com>
2022-12-11 21:50:07 +0000

    Removed redundant/useless FAQ entries

--- cogs/FAQ.py
+++ cogs/FAQ.py
@@ -24,12 +24,12 @@ class FAQ(commands.Cog):
     embed.set_footer(text = f'Requested by {interaction.user.name}. If the changelog is outdated, DM a fast audi r8#1180.')
     await interaction.response.send_message(embed = embed)
 
-  @commands.command()
-  async def betacommands(self, ctx):
-    embed = discord.Embed(title = 'Beta commands', description = f"Beta commands were certain commands that were included with the bot that only worked in a specific channel in a seperate server that only <@478212283701526529> had access to (Some were accessible on this server though). These commands were either upcoming commands or commands I'm testing. None of them were final and/or may never have made it to the normal command list. \n These are now redundant due to the `dev` branch. \n**Do not ask for an invite to that server**")
-    embed.set_footer(text=f'Requested by {ctx.message.author}')
-    await ctx.message.delete()
-    await ctx.send(embed=embed)
+#  @commands.command()
+#  async def betacommands(self, ctx):
+#    embed = discord.Embed(title = 'Beta commands', description = f"Beta commands were certain commands that were included with the bot that only worked in a specific channel in a seperate server that only <@478212283701526529> had access to (Some were accessible on this server though). These commands were either upcoming commands or commands I'm testing. None of them were final and/or may never have made it to the normal command list. \n These are now redundant due to the `dev` branch. \n**Do not ask for an invite to that server**")
+#    embed.set_footer(text=f'Requested by {ctx.message.author}')
+#    await ctx.message.delete()
+#    await ctx.send(embed=embed)
 
   @commands.command()
   async def suggestions(self, ctx):
@@ -38,12 +38,12 @@ class FAQ(commands.Cog):
     await ctx.message.delete()
     await ctx.send(embed = embed)
 
-  @commands.command()
-  async def outages(self, ctx):
-    embed = discord.Embed(title='Bot outages', description = f"~~The bot may be having some frequent outages because the site used to host the bot shares IPs with multiple people meaning that cloudfare blocks the bot from connecting to Discord becasue of how many people are using the API on the same IP.\n \nIf the bot goes offline, please ping or DM <@478212283701526529>~~ \nThis should no longer be an issue due to me migrating the bot to a new host. Outages will now mostly happen because of the server rebooting for whatever reason and the bot not starting on restart. Please DM or ping <@478212283701526529> in the event that it does go down.")
-    embed.set_footer(text = f'Requested by {ctx.message.author}')
-    await ctx.message.delete()
-    await ctx.send(embed = embed)
+#  @commands.command()
+#  async def outages(self, ctx):
+#    embed = discord.Embed(title='Bot outages', description = f"~~The bot may be having some frequent outages because the site used to host the bot shares IPs with multiple people meaning that cloudfare blocks the bot from connecting to Discord becasue of how many people are using the API on the same IP.\n \nIf the bot goes offline, please ping or DM <@478212283701526529>~~ \nThis should no longer be an issue due to me migrating the bot to a new host. Outages will now mostly happen because of the server rebooting for whatever reason and the bot not starting on restart. Please DM or ping <@478212283701526529> in the event that it does go down.")
+#    embed.set_footer(text = f'Requested by {ctx.message.author}')
+#    await ctx.message.delete()
+#    await ctx.send(embed = embed)
 
   @commands.command()
   async def github(self, ctx):
