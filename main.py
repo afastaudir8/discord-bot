ed0cc1ee95fd99e527f3e949775fca84e36ffe18
afastaudir8 <45905959+afastaudir8@users.noreply.github.com>
2022-12-11 02:17:09 -0500

    Update main.py

--- main.py
+++ main.py
@@ -60,13 +60,13 @@ async def on_ready():
   
 @client.event 
 async def on_member_remove(member):
- channel=client.get_channel(1005707501435629752)
+ channel=client.get_channel('CHANNELIDHERE')
  await channel.send(f'{member} has left the server! :(')
 
 @client.event
 async def on_member_join(member):
-    guild = client.get_guild(1005707501435629749)
-    channel=client.get_channel(1005707501435629752)
+    guild = client.get_guild('GUILDIDHERE')
+    channel=client.get_channel('CHANNELIDHERE')
     await channel.send(f"Hello {member.mention}! Welcome to this server! You are now the {guild.member_count}th member! \nBefore anything, I recommend you go to <#1008126343738830848> and familiarize yourself with the rules. \n<#1007345658136625203> contains the download link to the game (You can also get the link by using the changelog command). \nFinally, to learn more on the game, check out <#1010733249384960100> \nTo learn how to use this bot, use *help.")
 
 
