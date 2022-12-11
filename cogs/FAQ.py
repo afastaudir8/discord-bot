4dfc30355fc084c074817592bae73131da6834d9
afastaudir8 <ervd516@gmail.com>
2022-12-11 07:11:36 +0000

    Revert commits

--- cogs/FAQ.py
+++ cogs/FAQ.py
@@ -9,6 +9,21 @@ class FAQ(commands.Cog):
         self.client = client
 
   @commands.command()
+#  @commands.cooldown(2, 30, commands.BucketType.user)
+  async def about(self, ctx):
+    embed = discord.Embed(title='About the project', description = f'Check out <#1010733249384960100>\nThe pin in that channel is the first message. I highly recommend you read it.', color=0x001aff)
+    embed.set_footer(text = f'By the way, if you have any suggestions for the bot, let me know! Requested by {ctx.message.author}.')
+    await ctx.message.delete()
+    await ctx.send(embed=embed)
+
+  @commands.command()
+  async def changelog(self, ctx):
+    embed = discord.Embed(title= 'Download the game', description = f"**Version 17: The AI Update** \n -Damage direction arrows \n -Crosshair added \n -Plasma drone laser attack \n Plasma drone ai \n -Upgrades to AIs in drones \n -Shotgunners now fire all projectiles in the same frame \n -Optimization on projectiles makes them appear to have triple the projectiles they do \n -Explosions no longer crash game when damaging creator of explosion or actor of same team as creator \n NEW ISSUE: explosions will not damage these actors \n-Behind the scenes code nonsense \n-Kamanzie drones now can move around corners (at cost of not turning smoothly) \n-Plasma projectiles now have a texture, the ability to be parried, and no longer get stuck on surfaces, and damage the player \n -Hitscan parry (unknown issue stops it from working right now) \n-Made MP map look more interesting for testing movement \n-Running physics on ground now no longer make player slide about like a maniac (this will be re introduced to a significantly lower extent after landing from a jump)  \n **Coming soon** \n * Version 18 is releasing by the end of December.* \n-First fully modeled and textured gun \n-Fix issue where kantanna won't go away \n -Animate kantanna slash/parry \n -Super shotgun \n -Other stuff", url = 'https://drsupervillain.itch.io/ramprage', color=0x001aff)
+    embed.set_footer(text = f'Requested by {ctx.message.author}. If the changelog is outdated, DM a fast audi r8#1180.')
+    await ctx.message.delete()
+    await ctx.send(embed = embed)
+
+  @commands.command()
   async def betacommands(self, ctx):
     embed = discord.Embed(title = 'Beta commands', description = f"Beta commands were certain commands that were included with the bot that only worked in a specific channel in a seperate server that only <@478212283701526529> had access to (Some were accessible on this server though). These commands were either upcoming commands or commands I'm testing. None of them were final and/or may never have made it to the normal command list. \n These are now redundant due to the `dev` branch. \n**Do not ask for an invite to that server**")
     embed.set_footer(text=f'Requested by {ctx.message.author}')
@@ -32,7 +47,7 @@ class FAQ(commands.Cog):
   @commands.command()
   async def github(self, ctx):
     embed = discord.Embed(title = 'GitHub Repository', description = f'Ever since 9/12/2022 this bot has been open sourced on GitHub. Feel free to make a pull request on the dev branch!', url = 'https://github.com/afastaudir8/discord-bot')
-    embed.set_footer(text = f"Requested by {ctx.message.author}. Current bot version: 1.1.4.")
+    embed.set_footer(text = f"Requested by {ctx.message.author}. Current bot version: 1.2.0.")
     await ctx.message.delete()
     await ctx.send(embed = embed)
 
--- cogs/Useful.py
+++ cogs/Useful.py
@@ -23,10 +23,9 @@ class Useful(commands.Cog):
   @commands.command(brief = 'Shows credits for the bot.')
   async def credits(self, ctx):
     embed = discord.Embed(title = 'Credits', description = f'Bot created by <@478212283701526529> using the discord.py library. Other libraries include Translator, goslate, datetime, and asyncio \n Check out the source code here: https://github.com/afastaudir8/discord-bot', url='https://github.com/afastaudir8/discord-bot', colour=0x00FFF3)
-    embed.set_footer(text=f"Requested by {ctx.message.author}. Current bot version: 1.1.4.")
+    embed.set_footer(text=f"Requested by {ctx.message.author}. Current bot version: 1.2.0.")
     await ctx.message.delete()
     await ctx.send(embed=embed)
-
   @commands.command()
   async def suicidehotlines(self, ctx):
     embed = discord.Embed(title='Suicide Prevention Hotlines', description=f"You're not alone  \n We all care about you, please **do not** go through with it. \nCall one of the crisis hotlines listed in the Wikipedia article linked in the title depending on where you are. You could also ping Ash (<@708094727588937799>) for help.", url='https://en.wikipedia.org/wiki/List_of_suicide_crisis_lines', color=0xff0000)
