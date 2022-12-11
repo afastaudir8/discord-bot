a96eb260fa3807c16b12da17bfb67f52dd7c16e6
afastaudir8 <ervd516@gmail.com>
2022-12-11 23:57:19 +0000

    Added option to hide FAQ commands

--- cogs/FAQ.py
+++ cogs/FAQ.py
@@ -3,7 +3,8 @@ from discord.ext import commands
 from discord import app_commands
 import random
 from random import choice
-
+import typing
+Literal = typing.Literal
 class FAQ(commands.Cog):
 
   def __init__(self, client):
@@ -12,17 +13,27 @@ class FAQ(commands.Cog):
 #  @commands.command()
 #  @commands.cooldown(2, 30, commands.BucketType.user)
   @app_commands.command(name='about', description='About the RampRage project')
-  async def about(self, interaction: discord.Interaction):
+  @app_commands.describe(hide = "Hide the output?")
+  async def about(self, interaction: discord.Interaction, hide: Literal['Yes', 'No']):
     embed = discord.Embed(title='About the project', description = f'Check out <#1010733249384960100>\nThe pin in that channel is the first message. I highly recommend you read it.', color=0x001aff)
     embed.set_footer(text = f'By the way, if you have any suggestions for the bot, let me know! Requested by {interaction.user.name}.')
-    await interaction.response.send_message(embed=embed)
+    if hide == 'Yes':
+      hide = True
+    else:
+      hide = False
+    await interaction.response.send_message(embed=embed, ephemeral = hide)
 
 #  @commands.command()
   @app_commands.command(name='changelog', description='Changelog for the latest version of the game.')
-  async def changelog(self, interaction: discord.Interaction):
+  @app_commands.describe(hide = "Hide the output?")
+  async def changelog(self, interaction: discord.Interaction, hide: Literal['Yes', 'No']):
     embed = discord.Embed(title= 'Download the game', description = f"**Version 17: The AI Update** \n -Damage direction arrows \n -Crosshair added \n -Plasma drone laser attack \n Plasma drone ai \n -Upgrades to AIs in drones \n -Shotgunners now fire all projectiles in the same frame \n -Optimization on projectiles makes them appear to have triple the projectiles they do \n -Explosions no longer crash game when damaging creator of explosion or actor of same team as creator \n NEW ISSUE: explosions will not damage these actors \n-Behind the scenes code nonsense \n-Kamanzie drones now can move around corners (at cost of not turning smoothly) \n-Plasma projectiles now have a texture, the ability to be parried, and no longer get stuck on surfaces, and damage the player \n -Hitscan parry (unknown issue stops it from working right now) \n-Made MP map look more interesting for testing movement \n-Running physics on ground now no longer make player slide about like a maniac (this will be re introduced to a significantly lower extent after landing from a jump)  \n **Coming soon** \n * Version 18 is releasing by the end of December.* \n-First fully modeled and textured gun \n-Fix issue where kantanna won't go away \n -Animate kantanna slash/parry \n -Super shotgun \n -Other stuff", url = 'https://drsupervillain.itch.io/ramprage', color=0x001aff)
     embed.set_footer(text = f'Requested by {interaction.user.name}. If the changelog is outdated, DM a fast audi r8#1180.')
-    await interaction.response.send_message(embed = embed)
+    if hide == 'Yes':
+      hide = True
+    else:
+      hide = False
+    await interaction.response.send_message(embed = embed, ephemeral=hide)
 
 #  @commands.command()
 #  async def betacommands(self, ctx):
@@ -33,10 +44,15 @@ class FAQ(commands.Cog):
 
 #  @commands.command()
   @app_commands.command(name='suggestions', description='Tells you where to put suggestions')
-  async def suggestions(self, interaction: discord.Interaction):
+  @app_commands.describe(hide = 'Hide the output?')
+  async def suggestions(self, interaction: discord.Interaction, hide: Literal['Yes', 'No']):
     embed = discord.Embed(title = 'Suggesting commands/features for the bot', description = f"As the bot developer has quite literally zero ideas for commands to add, he'd appreciate any suggestions you send to him! \nJust DM <@478212283701526529>.")
     embed.set_footer(text=f'Requested by {interaction.user.name}. Meme/troll suggestions will be ignored.')
-    await interaction.response.send_message(embed = embed)
+    if hide == 'Yes':
+      hide =  True
+    else:
+      hide = False
+    await interaction.response.send_message(embed = embed, ephemeral=hide)
 
 #  @commands.command()
 #  async def outages(self, ctx):
@@ -47,15 +63,22 @@ class FAQ(commands.Cog):
 
 #  @commands.command()
   @app_commands.command(name='github', description='Shares the GitHub repo.')
-  async def github(self, interaction: discord.Interaction):
+  @app_commands.describe(hide = 'Hide the output?')
+  async def github(self, interaction: discord.Interaction, hide: Literal['Yes', 'No']):
     embed = discord.Embed(title = 'GitHub Repository', description = f'Ever since 9/12/2022 this bot has been open sourced on GitHub. Feel free to make a pull request on the dev branch!', url = 'https://github.com/afastaudir8/discord-bot')
     embed.set_footer(text = f"Requested by {interaction.user.name}. Current bot version: 2.0.0.")
-    await interaction.response.send_message(embed = embed)
+    if hide == 'Yes':
+      hide = True
+    else:
+      hide = False
+    await interaction.response.send_message(embed = embed, ephemeral=hide)
 
   @app_commands.command(name = 'slashcommands')
-  async def slashcommands(self, interaction: discord.Interaction):
+  @app_commands.describe(hide = 'Hide the output?')
+  async def slashcommands(self, interaction: discord.Interaction, hide: Literal ['Yes', 'No']):
     embed = discord.Embed(title = "Slash commands", description='Since 11/12/22, the bot has migrated to using exclusively slash commands.')
-    await interaction.response.send_message(embed = embed)
+    embed.set_footer(text = f'Reuqested by {interaction.user.name}')
+    await interaction.response.send_message(embed = embed, ephemeral=hide)
 
 
 async def setup(client):
